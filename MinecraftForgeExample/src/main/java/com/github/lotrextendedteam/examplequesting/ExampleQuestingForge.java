e4cd6f61bb70a981dd32cdff3891d9b07eec7b26
maximuslotro <61996354+maximuslotro@users.noreply.github.com>
2026-01-22 11:37:45 -0600

    Add example forge mod for proof of concept

---
+++ MinecraftForgeExample/src/main/java/com/github/lotrextendedteam/examplequesting/ExampleQuestingForge.java
@@ -0,0 +1,13 @@
+package com.github.lotrextendedteam.examplequesting;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("example_questing_forge")
+public class ExampleQuestingForge
+{
+
+    public ExampleQuestingForge() {
+    }
+
+}
